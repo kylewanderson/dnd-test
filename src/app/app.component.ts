5ab8d25ea5524680d0550a863ec692b6094ea56c
kylea <kylea@opin.com>
2018-09-21 14:59:23 -0500

    Working prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  questions = [
+    'Completion of high value rocks',
+    'Impact on My Team',
+    'Virtues and Values',
+    'Productivity and Measureable Results'
+  ];
+  workingQuestion = '';
+
+  @ViewChild('mainContainer') mainContainer: ElementRef;
+
+  addQuestion() {
+    this.questions.push(this.workingQuestion);
+    this.workingQuestion = '';
+  }
+
+  deleteQuestion(question) {
+    this.questions = this.questions.filter(x => x !== question);
+  }
+
+  downloadPdf() {
+    let html = this.mainContainer.nativeElement;
+    let doc = new jsPDF();
+    doc.fromHTML(html.innerHTML, 15, 15, {
+      'elementHandlers': (e) => console.log(e)
+    })
+
+    //doc.text('Snargle', 10, 10);
+    doc.save('test.pdf');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DndModule } from 'ng2-dnd';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DndModule.forRoot(),
+    FormsModule,
+    MaterialModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
